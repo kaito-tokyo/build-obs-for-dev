ebba1eae8f5407f3c7a94f2ffab818f3697c502b
Kaito Udagawa <umireon@kaito.tokyo>
2026-01-11 05:23:59 +0900

    Add build setup script and update clone scripts
    Introduced 30-build.zsh for target and build parameter setup. Updated clone-obs-deps.sh to copy the new build script, create a placeholder, and remove a macOS dependency script. Modified clone-obs-studio.sh to remove 'libobs-metal' from the CMake configuration.

--- clone-obs-deps.sh
+++ clone-obs-deps.sh
@@ -3,3 +3,7 @@ set -euo pipefail
 
 rm -rf obs-deps
 git clone https://github.com/obsproject/obs-deps.git
+
+echo > obs-deps/utils.zsh/functions/fix_rpaths
+cp 30-build.zsh obs-deps/utils.zsh/30-build.zsh
+rm obs-deps/deps.macos/90-syphon.zsh
--- clone-obs-studio.sh
+++ clone-obs-studio.sh
@@ -3,3 +3,5 @@ set -euo pipefail
 
 rm -rf obs-studio
 git clone --recursive https://github.com/obsproject/obs-studio.git
+
+perl -pi -e 's/add_subdirectory(libobs-metal)//' obs-studio/CMakeLists.txt
